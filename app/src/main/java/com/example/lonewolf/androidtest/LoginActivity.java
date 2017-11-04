a2d8392a834ef28e22c08e305e8ed5b53afcf31f
Daivat Vaishnani <daivatvaishnani18@gmail.com>
2017-11-04 18:56:37 +0530

    GuestActivity is added

--- app/src/main/java/com/example/lonewolf/androidtest/LoginActivity.java
+++ app/src/main/java/com/example/lonewolf/androidtest/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.lonewolf.androidtest;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -184,9 +185,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
+            Intent i = new Intent(LoginActivity.this, GuestActivity.class);
+            startActivity(i);
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+//            mAuthTask = new UserLoginTask(email, password);
+//            mAuthTask.execute((Void) null);
         }
     }
 
@@ -315,13 +318,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 return false;
             }
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
+            Intent i = new Intent(LoginActivity.this, GuestActivity.class);
+            startActivity(i);
+//            for (String credential : DUMMY_CREDENTIALS) {
+//                String[] pieces = credential.split(":");
+//                if (pieces[0].equals(mEmail)) {
+//                    // Account exists, return true if the password matches.
+//                    return pieces[1].equals(mPassword);
+//                }
+//            }
 
             // TODO: register the new account here.
             return true;
